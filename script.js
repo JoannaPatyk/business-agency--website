63432629e6958cf2508d66340dd35d1d3446811c
Joanna Patyk <patykjoanna93@gmail.com>
2023-06-27 17:30:50 +0200

    Changed UI
    Added counter of satisfied customers
    Changed heading font-family

--- script.js
+++ script.js
@@ -24,6 +24,34 @@ function showMenuButton() {
 menuBtn.addEventListener('click', showBackButton);
 backBtn.addEventListener('click', showMenuButton);
 
+// Section 2
+
+const items = [...document.querySelectorAll('.number')];
+
+const updateCount = (el) => {
+    const value = parseInt(el.dataset.value);
+    const increment = Math.ceil(value / 1000);
+    // const increment = 1;
+    let initialValue = 0;
+
+    const increaseCount = setInterval(() => {
+        initialValue += increment;
+
+        if (initialValue > value) {
+            el.textContent = `${value}+`;
+            clearInterval(increaseCount);
+            return;
+        }
+
+        el.textContent = `${initialValue}+`;
+    }, 1);
+    // console.log(increaseCount);
+};
+
+items.forEach((item) => {
+    updateCount(item);
+});
+
 // Section 3
 
 const swiper = new Swiper('.swiper', {
@@ -32,9 +60,9 @@ const swiper = new Swiper('.swiper', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-        rotate: 80,
+        rotate: 0,
         stretch: 0,
-        depth: 100,
+        depth: 50,
         modifier: 1,
         slideShadows: true
     }
